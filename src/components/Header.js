c6e0bfe22f609fa13a19ffb8eab37ec14e2549e2
Justin Young <jyoung7223@yahoo.com>
2021-02-13 13:27:20 -0700

    Trying HashRouter instead of BrowserRouter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import {Link} from "react-router-dom";
+import {HashLink as Link} from "react-router-dom";
 
 function Header(){
     const initHeader = {
