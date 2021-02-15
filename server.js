b0c6a362c4b337f08cb9d8dbeb4da0e20ddf85ab
Justin Young <jyoung7223@yahoo.com>
2021-02-15 12:17:13 -0700

    trying to fix heroku errors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 function routes(req, res){
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 }
 
 // Add routes, both API and view
